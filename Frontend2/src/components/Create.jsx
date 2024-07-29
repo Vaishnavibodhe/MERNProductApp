11cb4846c5487373b040494ec5118455279f1f8a
vaishnavi bodhe <vaishnavibodhe0@gmail.com>
2024-07-29 17:37:57 +0530

    jaya

--- Frontend2/src/components/Create.jsx
+++ Frontend2/src/components/Create.jsx
@@ -26,7 +26,7 @@ formData.append('bgcolor', bgcolor);
 formData.append('panelcolor', panelcolor);
 formData.append('textcolor', textcolor);
 
-axios.post("http://localhost:4005/create",formData)
+axios.post("https://mern-product-app-xak2.vercel.app/create",formData)
 .then(result => {
   console.log(result)
     navigate("/alldata")
--- Frontend2/src/components/Navbar.jsx
+++ Frontend2/src/components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
   const navigate=useNavigate();
 
  const handleLogout=()=>{
-    axios.get("http://localhost:4005/logout")
+    axios.get("https://mern-product-app-xak2.vercel.app/logout")
     .then(res=> {
       if(res.data=== "success")
       alert("user logout");
