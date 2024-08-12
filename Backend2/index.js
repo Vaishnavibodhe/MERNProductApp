9eab35b922fc88887df1c3940b5bb14a75ac9809
vaishnavi bodhe <vaishnavibodhe0@gmail.com>
2024-08-12 09:38:30 +0530

    why

--- Backend2/index.js
+++ Backend2/index.js
@@ -1,29 +1,28 @@
 const express =require("express");
 const app=express();
 const mongoose=require("mongoose");
-
 const UserRouter=require("./routes/UserRouter")
-const CreateRouter=require("./routes/CreateRouter")
 const path = require("path");
 const cors=require("cors");
 const cookieParser=require("cookie-parser");
 const dotenv=require("dotenv");
 dotenv.config();
 
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/Images', express.static(path.join(__dirname, 'Public/Images'))); // Serve static files
 
 const corsOptions = {
     origin: ["http://localhost:5173"],
-    methods: ["GET", "POST", "PUT", "DELETE"], 
+    methods: ["GET", "POST",  "DELETE"], 
     credentials: true 
   };
   
   app.use(cors(corsOptions));
-  app.use(cookieParser());
+ 
 
-    mongoose.connect(process.env.URI,{ useNewUrlParser: true, useUnifiedTopology: true })
+    mongoose.connect(process.env.URI)
     .then(()=>{
         console.log("mongodb connected");
     
@@ -36,11 +35,9 @@ const corsOptions = {
     console.log("connecting to MongoDB:",err)
 })
 
-app.get("/",(req,res)=>{
-res.send("helloworld")
-})
+
 app.use(UserRouter);
-app.use(CreateRouter);
+
 
 
 
--- Backend2/routes/UserRouter.js
+++ Backend2/routes/UserRouter.js
@@ -2,11 +2,15 @@ const express=require("express");
 const app=express();
 const router=express.Router();
 const UserModel =require("../models/UserModel");
+const CreateModel = require("../models/CreateModel");
 const bcrypt=require("bcrypt");
 const jwt=require("jsonwebtoken");
 const cookieParser=require("cookie-parser")
+const multer = require("multer");
+const path = require("path");
 
 app.use(cookieParser());
+app.use(express.json());
 
 /*for sign up*/
 router.post("/signup",(req,res)=>{
@@ -29,8 +33,8 @@ router.post("/login",(req,res)=>{
        if(user){
         bcrypt.compare(password,user.password,(err,response)=>{
             if(response){
-                const token=jwt.sign({email:user.email},"secret",{expiresIn:"1d"})
-                res.cookie("token",token,{ httpOnly: true, secure: false });
+                const token=jwt.sign({email:user.email}, "secret",{expiresIn:'1h'})
+                res.cookie("token",token, {httpOnly: true,secure: false });
             return res.json("success")
             }else{ 
                return res.json("passwor is incorrect")
@@ -71,7 +75,65 @@ const verifyUser = (req, res, next) => {
 /*logout*/
 router.get("/logout",(req,res)=>{
     res.clearCookie("token");
-    return res.json("success")
+  return res.json("success")
 })
-//
+// Store file for image
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "Public/Images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.fieldname + "_" + Date.now() + path.extname(file.originalname));
+  }
+});
+
+const upload = multer({
+  storage: storage
+});
+
+//creating  products
+
+router.post("/create", verifyUser, upload.single('file'), (req, res) => {
+  if (!req.file) {
+    return res.status(400).json({ message: "No file uploaded" });
+  }
+
+  CreateModel.create({
+    file: req.file.filename,
+    name: req.body.name,
+    price: req.body.price,
+    discount: req.body.discount,
+    bgcolor: req.body.bgcolor,
+    panelcolor: req.body.panelcolor,
+    textcolor: req.body.textcolor
+  })
+  .then(result => res.json(result))
+  .catch(err => res.status(500).json({ error: 'Failed to create product' }));
+});
+
+// Get Products
+router.get("/getproducts", (req, res) => {
+  CreateModel.find({})
+    .then(products => res.json(products))
+    .catch(err => res.status(500).json({ error: 'Failed to fetch products' }));
+});
+router.get("/getproductbyid/:id",(req,res)=>{
+  const {id }= req.params;
+  CreateModel.findById(id)
+  .then(product => {
+    if (!product) {
+      return res.status(404).json({ error: 'Product not found' });
+    }
+    res.json(product);
+  })
+  .catch(err => res.status(500).json({ error: 'Failed to fetch product' }));
+})
+/*delete product by id*/
+router.delete("/deleteproduct/:id",(req,res)=>{
+  const {id} =req.params;
+  CreateModel.findByIdAndDelete(id)
+  .then(result => res.json("success"))
+  .catch(err => res.json(err))
+})
+
 module.exports=router;
